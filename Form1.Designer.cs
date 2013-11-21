12b0fdb2aacb9497a13fdfec261c05964a4961ae
Andrew Manalo <acmanalo@gmail.com>
2013-11-21 01:55:46 -0800

    Completed
    Complete version of FriendBirthdayWriter

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace FriendBirthdayWriter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.LastNameTextBox = new System.Windows.Forms.TextBox();
+            this.EmailAddressTextBox = new System.Windows.Forms.TextBox();
+            this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BirthDayTextBox = new System.Windows.Forms.TextBox();
+            this.BirthMonthTextBox = new System.Windows.Forms.TextBox();
+            this.CreateEntryButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FirstNameTextBox
+            // 
+            this.FirstNameTextBox.Location = new System.Drawing.Point(106, 24);
+            this.FirstNameTextBox.Name = "FirstNameTextBox";
+            this.FirstNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.FirstNameTextBox.TabIndex = 0;
+            // 
+            // LastNameTextBox
+            // 
+            this.LastNameTextBox.Location = new System.Drawing.Point(106, 50);
+            this.LastNameTextBox.Name = "LastNameTextBox";
+            this.LastNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.LastNameTextBox.TabIndex = 1;
+            // 
+            // EmailAddressTextBox
+            // 
+            this.EmailAddressTextBox.Location = new System.Drawing.Point(106, 76);
+            this.EmailAddressTextBox.Name = "EmailAddressTextBox";
+            this.EmailAddressTextBox.Size = new System.Drawing.Size(100, 20);
+            this.EmailAddressTextBox.TabIndex = 2;
+            // 
+            // PhoneNumberTextBox
+            // 
+            this.PhoneNumberTextBox.Location = new System.Drawing.Point(106, 102);
+            this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+            this.PhoneNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PhoneNumberTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Last Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Email Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Phone #:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Birth Month:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 158);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Birth Day:";
+            // 
+            // BirthDayTextBox
+            // 
+            this.BirthDayTextBox.Location = new System.Drawing.Point(106, 154);
+            this.BirthDayTextBox.Name = "BirthDayTextBox";
+            this.BirthDayTextBox.Size = new System.Drawing.Size(100, 20);
+            this.BirthDayTextBox.TabIndex = 5;
+            // 
+            // BirthMonthTextBox
+            // 
+            this.BirthMonthTextBox.Location = new System.Drawing.Point(106, 128);
+            this.BirthMonthTextBox.Name = "BirthMonthTextBox";
+            this.BirthMonthTextBox.Size = new System.Drawing.Size(100, 20);
+            this.BirthMonthTextBox.TabIndex = 4;
+            // 
+            // CreateEntryButton
+            // 
+            this.CreateEntryButton.Location = new System.Drawing.Point(87, 193);
+            this.CreateEntryButton.Name = "CreateEntryButton";
+            this.CreateEntryButton.Size = new System.Drawing.Size(75, 23);
+            this.CreateEntryButton.TabIndex = 12;
+            this.CreateEntryButton.Text = "Create Entry";
+            this.CreateEntryButton.UseVisualStyleBackColor = true;
+            this.CreateEntryButton.Click += new System.EventHandler(this.CreateEntryButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 245);
+            this.Controls.Add(this.CreateEntryButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BirthDayTextBox);
+            this.Controls.Add(this.BirthMonthTextBox);
+            this.Controls.Add(this.PhoneNumberTextBox);
+            this.Controls.Add(this.EmailAddressTextBox);
+            this.Controls.Add(this.LastNameTextBox);
+            this.Controls.Add(this.FirstNameTextBox);
+            this.Name = "Form1";
+            this.Text = "Friend Birthday Recorder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FirstNameTextBox;
+        private System.Windows.Forms.TextBox LastNameTextBox;
+        private System.Windows.Forms.TextBox EmailAddressTextBox;
+        private System.Windows.Forms.TextBox PhoneNumberTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox BirthDayTextBox;
+        private System.Windows.Forms.TextBox BirthMonthTextBox;
+        private System.Windows.Forms.Button CreateEntryButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿/* This program was written to practice writing to .csv files
+ * and to complement the FriendBirthdayReader that reads .csv files
+ * provided in class. */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;                 // (don't forget to include this when dealing with file reading / writing)
+
+namespace FriendBirthdayWriter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FirstNameTextBox.Focus();           // give the first name text box focus on startup
+        }
+
+        private void CreateEntryButton_Click(object sender, EventArgs e)
+        {
+            const string PATH = "FriendRecord.csv";    // Specify the file to which you want to write.
+            
+            // Declare the variables where you will store the entries of the form
+            string firstName, lastName, emailAddress, phoneNumber, birthMonth, birthDay;
+
+            // Grab the entries of the form and store them in the variables we declared
+            firstName =     FirstNameTextBox.Text;
+            lastName =      LastNameTextBox.Text;
+            emailAddress =  EmailAddressTextBox.Text;
+            phoneNumber =   PhoneNumberTextBox.Text;
+            birthMonth =    BirthMonthTextBox.Text;
+            birthDay =      BirthDayTextBox.Text;
+
+            // (I got to this point and thought, "Let's create a method for writing to our .csv")
+
+            // Pack up the variables into an array because method expects a string array
+            // Declare a string array of size 6 to contain all 6 variables we grabbed from the form
+            string[] FriendData = new string[6];
+
+            // Fill the entires of the array
+            FriendData[0] = firstName;
+            FriendData[1] = lastName;
+            FriendData[2] = emailAddress;
+            FriendData[3] = phoneNumber;
+            FriendData[4] = birthMonth;
+            FriendData[5] = birthDay;
+
+            // Pass the path name and fields array to our WriteToCSV method
+            WriteToCSV(PATH, FriendData);                   // (See below for how the method works)
+
+// (This section is not necessary, you can compoletely remove it if you want and the whole application would still work)
+            // This whole string builder idea is explained in the WriteToCSV method
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (string field in FriendData)
+            {
+                if (stringBuilder.Length > 0)
+                    stringBuilder.Append(",");
+                stringBuilder.Append(field);
+            }
+
+            MessageBox.Show("\" " + stringBuilder.ToString() + " \" was added to the .csv.");
+// End of uneccessary section=========================================================================================
+
+            // Prepare for the next friend entry
+            // Clear all the text boxes
+            FirstNameTextBox.Text = "";
+            LastNameTextBox.Text = "";
+            EmailAddressTextBox.Text = "";
+            PhoneNumberTextBox.Text = "";
+            BirthMonthTextBox.Text = "";
+            BirthDayTextBox.Text = "";
+
+            // Move the focus back to the first name text box
+            FirstNameTextBox.Focus();
+        }
+
+        // Method for writing to our .csv
+        void WriteToCSV(string path, string[] fields)
+        {
+            const string DELIM = ",";                       // Indicate the delimeter of the file
+
+            // Create a stream writer to allow us to write to our .csv
+            // Give it the specified path/file name for where to create or look for this .csv
+            // The true parameter indicates that if the file exists, append to the file and if it doesn't create it
+            StreamWriter streamWriter = new StreamWriter(path, true);
+
+            // Create a string builder to build our comma delimited for our .csv
+            StringBuilder stringBuilder = new StringBuilder();
+
+            // Loop through each entry in the fields array and write it to this single string that we're building
+            // each entry will be separated by the delimiter, which in our case is a ","
+            foreach (string field in fields)
+            {
+                // this if statement checks to see if this is the first entry we're putting into our string builder
+                // if it isn't the first string, we append a delimiter to our string before writing our next string
+                if (stringBuilder.Length > 0)
+                    stringBuilder.Append(DELIM);
+
+                // append the current entry to the string builder
+                stringBuilder.Append(field);
+            }
+
+            // (At this point our string similar "John,Smith,12345555,js@gmail.com,11,20" is written in stringBuilder)
+            // Write stringBuilder it to .csv
+            streamWriter.WriteLine(stringBuilder.ToString());
+
+            // Close the stream
+            streamWriter.Close();
+        }
+    }
+}
